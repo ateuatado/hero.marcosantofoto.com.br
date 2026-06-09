03c9c6987b095ab956f2e2d457c546dfb1d06c19
OVP-SP <ovp@cehal.org.br>
2026-06-09 18:15:56 -0300

    fix: remove endif orfao que causava ParseError na hero_page

--- app/Views/hero_page.php
+++ app/Views/hero_page.php
@@ -477,9 +477,9 @@
         </div>
     </div>
 </div>
-<?php endif; ?>
 
 <?= $this->endSection() ?>
+
 
 <?= $this->section('scripts') ?>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
